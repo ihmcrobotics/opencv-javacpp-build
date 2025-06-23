6c60e2553c14f19cbbfa8cf548a55b4943dfded5
Dexton Anderson <danderson@ihmc.org>
2025-06-23 17:03:32 -0500

    Copy artifacts out of container

--- build-javacpp-no-cache.sh
+++ build-javacpp-no-cache.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-docker build -f javacpp.Dockerfile --progress=plain -t opencv-arm64-cuda-cross-javacpp . --no-cache
+docker build -f javacpp.Dockerfile --progress=plain -t opencv-arm64-cuda-cross-javacpp . 
+docker create --name opencv-arm64-cuda-cross-javacpp-temp opencv-arm64-cuda-cross-javacpp
+rm -rf ./install
+mkdir ./install
+docker cp opencv-arm64-cuda-cross-javacpp-temp:/root/.m2 ./install
+docker rm opencv-arm64-cuda-cross-javacpp-temp
