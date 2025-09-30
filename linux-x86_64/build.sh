7d525920193b025f043ce61871a765babe3150ef
Dexton Anderson <danderson@ihmc.org>
2025-09-30 18:57:08 +0000

    Workflow changes

--- linux-x86_64/build.sh
+++ linux-x86_64/build.sh
@@ -15,7 +15,7 @@ cp ../cppbuild_1.5.11.sh.diff .
 cp $REPO_DIR/linux-x86_64/javacpp.Dockerfile .
 cp $REPO_DIR/linux-x86_64/opencv-cudnn-version.patch .
 cp $REPO_DIR/linux-x86_64/private.cuda.hpp.diff .
-cp $REPO_DIR/linux-x86_64/OpenCVDetectCUDAUtils.diff .
+cp $REPO_DIR/linux-x86_64/OpenCVDetectCUDAUtils.cmake.diff .
 
 docker build -f javacpp.Dockerfile --progress=plain --no-cache -t linux-x86_64-opencv-javacpp .
 docker create --name linux-x86_64-opencv-javacpp-temp linux-x86_64-opencv-javacpp
